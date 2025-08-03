e41b71a41e11af76a3b1bd73ecab563eda9ef010
Steve Austin <steve@austin.com>
2025-08-03 16:41:26 +0100

    Updated build and run script to access port 200

--- build_run.sh
+++ build_run.sh
@@ -1,3 +1,4 @@
 docker build -t flask-nginx-app .
 #docker run -p 80:80 flask-nginx-app
-docker run -p 127.0.0.1:2000:80 flask-nginx-app
+#docker run -p 127.0.0.1:2000:80 flask-nginx-app
+docker run -p 2000:80 flask-nginx-app
